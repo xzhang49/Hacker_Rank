5eac3e248711f74efe5b4a06f55867adb8f4217d
xzhang49 <xzhanguh@gmail.com>
2022-10-04 19:16:22 -0500

    2nd commit

---
+++ React_Date_API/src/tests/App.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import expect from "expect";
+import App from "../App";
+import API from "../services/dateAPI";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { mount } from "enzyme";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+configure({ adapter: new Adapter() });
+
+describe("DateAPI <App />", () => {
+  it("complete app renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+  });
+
+  it("check if service is used to fetch data from the API", async () => {
+    const mockAdapter = new MockAdapter(axios);
+    mockAdapter.onGet("https://jsonmock.hackerrank.com/datetime").reply(200, {
+      time: "11:41:59 AM",
+      date: "08-10-2018"
+    });
+    let res = await API.getAPIResponse();
+    expect(res).toEqual("08-10-2018");
+  });
+
+  it("check if button click splits and displays the date", async () => {
+    const mockAdapter = new MockAdapter(axios);
+    mockAdapter.onGet("https://jsonmock.hackerrank.com/datetime").reply(200, {
+      time: "19:13:23 AM",
+      date: "10-09-2011"
+    });
+    const wrapper = mount(<App />);
+    expect(wrapper.text()).toEqual(
+      "Date APIDisplay DateDay: 9Month: 10Year: 2011"
+    );
+  });
+});
